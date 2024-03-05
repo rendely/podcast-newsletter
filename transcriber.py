60c858072887cae0a6d669567507a45b5f8007e3
matthew rendely <matthew.rendely@gmail.com>
2024-03-04 22:21:59 -0800

    Check for existing transcript file before transcribing

--- transcriber.py
+++ transcriber.py
@@ -5,9 +5,16 @@ class Transcriber:
     def __init__(self, file_name:str):
         self.file_name:str = file_name
         self.transcript:str|None = None
+        self.transcribe()
         self.run_whisper_cmd:str = f'whisper-cpp/main -f whisper-cpp/data/{file_name}.wav -m whisper-cpp/models/ggml-tiny.en.bin -of whisper-cpp/data/{file_name} -otxt'
     
+    
     def transcribe(self):
-        subprocess.run(self.run_whisper_cmd.split(' '))
-        with open(f'whisper-cpp/data/{self.file_name}.txt', 'r') as f:
-            self.transcript = f.read()
+        try:
+            with open(f'whisper-cpp/data/{self.file_name}.txt', 'r') as f:
+                self.transcript = f.read()
+
+        except FileNotFoundError:    
+            subprocess.run(self.run_whisper_cmd.split(' '))
+            with open(f'whisper-cpp/data/{self.file_name}.txt', 'r') as f:
+                self.transcript = f.read()
