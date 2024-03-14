343ff883d48c1734b48c48bc0373722cf785672d
matthew rendely <matthew.rendely@gmail.com>
2024-03-13 21:59:10 -0700

    Consisten naming in main

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     example_url = 'https://chrt.fm/track/F62CA2/podcasts.captivate.fm/media/9f0b8b67-02c1-495e-8d87-d90f1e273059/Hasan-Piker-MF.mp3'
     d = Downloader(url=example_url)
     t = Transcriber(file_name = d.file_name)
-    pod1 = Summarizer(transcript=t.transcript, service='ollama')
-    pod1.summarize()
+    s = Summarizer(transcript=t.transcript, service='ollama')
+    s.summarize()
