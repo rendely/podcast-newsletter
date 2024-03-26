649f3c9fefc7676b262ef5756e18704168274a59
matthew rendely <matthew.rendely@gmail.com>
2024-03-25 21:00:47 -0700

    Add json caching for summary chunks

--- summarizer.py
+++ summarizer.py
@@ -1,4 +1,6 @@
 from llm import LLM
+import os
+import json
 
 CHAR_TO_TOKEN:float = 4.3  # approx based on sampling 92362 / 21340
 # MAX_TOKENS:int = 16385  # for chat gpt 3.5 turbo
@@ -10,7 +12,7 @@ SPLIT_SIZE:int = round(CHAR_TO_TOKEN * MAX_TOKENS * SAFETY_FACTOR)
 
 # SUMMARIZE_PROMPT:str = '''You are an AI designed to summarize podcast transcript summaries. When a list of bullet points summarizing a podcast is provided, please organize it into topics, remove any duplicate bullets, but preserve all the information from the summaries to output a clean new final summary.'''
 
-SUMMARIZE_PROMPT:str = '''You are an AI designed to write email newsletters based on podcast transcripts. The user will provide you bullet points from a transcript of a podcast. Please make this more readable by inserting headings to separate the different topics in the notes and combine repetitive bullet points to avoid duplicates. Try to write in the final style of an email newsletter to help somebody read the content of the podcast.'''
+SUMMARIZE_PROMPT:str = '''You are an AI designed to write email newsletters based on podcast transcripts. The user will provide you bullet points from a transcript of a podcast. Re-write this in prose as if by an enthusiastic author writing directly to the reader.'''
 
 BULLET_PROMPT:str = '''You are an AI designed to summarize podcast transcripts with relevant extracts. When raw podcast transcripts are sent to you by the user you will respond with a detailed 10 bullet point summary that preserves details of what was said, try to be more extractive instead of abstractive.'''
 
@@ -18,12 +20,13 @@ BULLET_PROMPT:str = '''You are an AI designed to summarize podcast transcripts w
 class Summarizer:
     ''' Class for summarizing podcast transcripts'''
 
-    def __init__(self, transcript: str, service:str):
+    def __init__(self, transcript: str, service:str, file_name:str|None):
         self.transcript: str = transcript
         self.transcript_chunks: list[str] = []
         self.create_chunks()
         self.summary_chunks: list[str] = []
         self.final_summary: str | None = None
+        self.file_name: str = file_name
         self.llm = LLM(service=service)
 
     def create_chunks(self):
@@ -39,7 +42,7 @@ class Summarizer:
 
         print('Generating summary...')
         self.summarize_chunks()
-        print(f'''Creating final summary:\n\n\n\n\n''')
+        print(f'''Creating final summary:\n\n''')
 
         completion = self.llm.completion(
             SUMMARIZE_PROMPT,
@@ -52,11 +55,21 @@ class Summarizer:
         return self.final_summary
 
     def summarize_chunks(self):
+        sum_chunks_file_name = f'{self.file_name}-sum-chunks.json'
+        if sum_chunks_file_name in os.listdir():
+            print('Loading from file')
+            with open(sum_chunks_file_name, 'r') as f:
+                self.summary_chunks = json.loads(f.read())
+            return
+        
         for i,chunk in enumerate(self.transcript_chunks):
             print(f'''Summarizing chunk {i+1} of {len(self.transcript_chunks)}...''')
             self.summary_chunks.append(
                 self.summarize_chunk(chunk)
             )
+        
+        with open(sum_chunks_file_name, 'w') as f:
+                f.write(json.dumps(self.summary_chunks))
 
     def summarize_chunk(self, chunk: str) -> str:
         completion = self.llm.completion(
