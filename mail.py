ea664f9d7d787e9119f950613a64d00b28402c68
matthew rendely <matthew.rendely@gmail.com>
2024-03-27 21:01:31 -0700

    Refactor mail into class and add to main.py

--- mail.py
+++ mail.py
@@ -4,24 +4,28 @@ import os
 MAIL_KEY = os.environ.get("MAIL_KEY")
 MAIL_DOMAIN = os.environ.get("MAIL_DOMAIN")
 TO_EMAIL = os.environ.get("TO_EMAIL")
-
 with open('email.html', 'r') as f:
     EMAIL_HTML = f.read()
 
-with open('a49a507f9dd765f4e7d5b6b9f928ff433e069a648b75f1cbb0658304089d727b-final-summary.txt', 'r') as f:
-   EMAIL_BODY = f.read()
+class Mail:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        with open(f'{self.file_name}-final-summary.txt', 'r') as f:
+            self.EMAIL_BODY = f.read()
+
+    def send(self):
+        print('Send email')
+        url = f'https://api.mailgun.net/v3/{MAIL_DOMAIN}/messages'
+        auth = ('api', MAIL_KEY)
+        data = {
+        'from': f'Pawdcasts Newsletter <postmaster@{MAIL_DOMAIN}>',
+        'to': [TO_EMAIL],
+        'subject': 'Pawdcast Newsletter',
+        'html': EMAIL_HTML.replace('$BODY_TEXT', split_text_to_p(self.EMAIL_BODY))
+        }
+        response = requests.post(url, auth=auth, data=data)
+        print(response.text)
 
 def split_text_to_p(text):
     ps = text.split('\n')
     return '\n'.join([f'<p>{p}</p>' for p in ps])
-
-url = f'https://api.mailgun.net/v3/{MAIL_DOMAIN}/messages'
-auth = ('api', MAIL_KEY)
-data = {
-'from': f'Pawdcasts Newsletter <postmaster@{MAIL_DOMAIN}>',
-'to': [TO_EMAIL],
-'subject': 'Pawdcast Newsletter: Ezra Klein and Paul Krugman',
-'html': EMAIL_HTML.replace('$BODY_TEXT', split_text_to_p(EMAIL_BODY))
-}
-response = requests.post(url, auth=auth, data=data)
-print(response.text)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from downloader import Downloader
 from summarizer import Summarizer
 from transcriber import Transcriber
+from mail import Mail 
 
 if __name__ == '__main__':
     example_url = 'https://pdst.fm/e/media.transistor.fm/f9a6fd57/c5a738c3.mp3'
@@ -8,3 +9,5 @@ if __name__ == '__main__':
     t = Transcriber(file_name = d.file_name)
     s = Summarizer(transcript=t.transcript, service='openai', file_name=t.file_name)
     s.summarize()
+    m = Mail(file_name=t.file_name)
+    m.send()
