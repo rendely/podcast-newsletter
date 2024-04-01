fedf7a8a9a222844d00f138218ad5c2aaa267dc4
matthew rendely <matthew.rendely@gmail.com>
2024-04-01 16:09:49 -0700

    Switch to anthropic as llm provider, great results but slower

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from transcriber import Transcriber
 from mail import Mail 
 
 if __name__ == '__main__':
-    example_url = 'https://pdst.fm/e/media.transistor.fm/f9a6fd57/c5a738c3.mp3'
+    example_url = 'https://traffic.libsyn.com/secure/wakingup/Making_Sense_361_Will_MacAskill_subscriber.mp3?dest-id=480596'
     d = Downloader(url=example_url)
     t = Transcriber(file_name = d.file_name)
-    s = Summarizer(transcript=t.transcript, service='openai', file_name=t.file_name)
+    s = Summarizer(transcript=t.transcript, service='anthropic', file_name=t.file_name)
     s.summarize()
     m = Mail(file_name=t.file_name)
     m.send()
